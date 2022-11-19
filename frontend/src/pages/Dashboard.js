2a4944232f2f9987742cfd8672c88e43683c2cf5
Brian Shimkus <brianshimkus1@gmail.com>
2022-11-18 23:40:49 -0600

    Restrict access to dahboard if not logged in

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,3 +1,18 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+
 export default function Dashboard() {
+	const navigate = useNavigate()
+	const dispatch = useDispatch()
+
+	const { user } = useSelector((state) => state.auth)
+
+	useEffect(() => {
+		if (!user) {
+			navigate('/login')
+		}
+	}, [user, navigate])
+
 	return <div>Dashboard</div>
 }

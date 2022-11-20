2d1bbe6150db6bd138885739b51adc3230bcf745
Brian Shimkus <brianshimkus1@gmail.com>
2022-11-20 16:31:57 -0600

    Displayed goals in dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -4,13 +4,16 @@ import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
 import Spinner from '../components/Spinner'
 import { getGoals, reset } from '../features/goals/goalSlice'
+import GoalItem from '../components/GoalItem'
 
 export default function Dashboard() {
 	const navigate = useNavigate()
 	const dispatch = useDispatch()
 
 	const { user } = useSelector((state) => state.auth)
-	const { isLoading, isError, message } = useSelector((state) => state.goals)
+	const { goals, isLoading, isError, message } = useSelector(
+		(state) => state.goals
+	)
 
 	useEffect(() => {
 		if (isError) {
@@ -40,6 +43,18 @@ export default function Dashboard() {
 			</section>
 
 			<GoalForm />
+
+			<section className='content'>
+				{goals.length > 0 ? (
+					<div className='goals'>
+						{goals.map((goal) => (
+							<GoalItem key={goal._id} goal={goal} />
+						))}
+					</div>
+				) : (
+					<h3>You have not set any goals</h3>
+				)}
+			</section>
 		</>
 	)
 }
